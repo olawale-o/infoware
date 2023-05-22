3ce322d0bf62afa9885c6b1216a66b44e8b45cf2
olawale-o <omoogunolawale@gmail.com>
2023-05-22 20:12:27 +0100

    Implement employees handler actions

--- src/v1/employees/handler.js
+++ src/v1/employees/handler.js
@@ -1,9 +1,64 @@
-exports.new = (req, res, next) => {};
+const service = require('./service');
+
+exports.new = async (req, res, next) => {
+  try {
+    const employee = await service.new(req.body);
+    if (!employee) {
+      return res.status(400).json({
+        status: 'error',
+        message: 'Error creating employee',
+      });
+    }
+    return res.status(201).json({
+      status: 'success',
+      data: employee,
+    });
+  } catch(e) {
+    console.log(e);
+    next(e);
+  }
+};
 
 exports.index = (req, res, next) => {};
 
-exports.show = (req, res, next) => {};
+exports.show = async (req, res, next) => {
+  try {
+    const employee = await service.show(parseInt(req.params.id));
+    if (!employee) {
+      return res.status(404).json({
+        status: 'error',
+        message: 'Employee not found',
+      });
+    }
+    return res.status(200).json({
+      status: 'success',
+      data: employee,
+    });
+  } catch(e) {
+    next(e);
+  }
+};
 
-exports.update = (req, res, next) => {};
+exports.update = async (req, res, next) => {
+  try {
+    const employee = await service.update(parseInt(req.params.id), req.body);
+    return res.status(204).json({
+      status: 'success',
+      data: employee,
+    });
+  } catch(e) {
+    next(e);
+  }
+};
 
-exports.delete = (req, res, next) => {};
+exports.delete = async (req, res, next) => {
+  try {
+    const employee = await service.delete(parseInt(req.params.id));
+    return res.status(204).json({
+      status: 'success',
+      data: employee,
+    });
+  } catch(e) {
+    next(e);
+  }
+};
