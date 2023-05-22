94afece7d1854ef238f101fd7dcdf42f845b1494
olawale-o <omoogunolawale@gmail.com>
2023-05-22 20:13:19 +0100

    Modify config folder

---
+++ src/config/config.js
@@ -0,0 +1,10 @@
+const config = require('../../config');
+module.exports = {
+  development: {
+    username: config.get('db.user.name'),
+    password: config.get('db.user.password'),
+    database: config.get('db.name'),
+    host: config.get('db.host'),
+    dialect: "mysql"
+  },
+}
