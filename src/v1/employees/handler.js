fd32f124265db1e9f49af26c92e64c5be3e25365
olawale-o <omoogunolawale@gmail.com>
2023-05-22 17:08:26 +0100

    Create employees required handler actions

---
+++ src/v1/employees/handler.js
@@ -0,0 +1,9 @@
+exports.new = (req, res, next) => {};
+
+exports.index = (req, res, next) => {};
+
+exports.show = (req, res, next) => {};
+
+exports.update = (req, res, next) => {};
+
+exports.delete = (req, res, next) => {};
