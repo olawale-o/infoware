7b2e57818fa438690bd690d9c9c8f36c17a011a0
olawale-o <omoogunolawale@gmail.com>
2023-05-23 09:02:35 +0100

    Modify employee test data

--- src/v1/employees/employee.data.js
+++ src/v1/employees/employee.data.js
@@ -1,33 +1,6 @@
 const {faker} = require('@faker-js/faker');
+const {createRandomEmployeeWithContacts } = require('../../common/data');
 
-const employeesData = [];
-const contacts = [];
-
-for (let i = 0; i < 3; i++) {
-  contacts.push(
-    {
-      contactName: faker.person.fullName(),
-      relationship: faker.helpers.arrayElement(['friend', 'family', 'colleague', 'wife', 'husband']),
-      phoneNumber: faker.phone.number(),
-      contactType: faker.helpers.arrayElement(['primary', 'secondary']),
-    }
-  );
-}
-
-for (let i = 0; i < 6; i++) {
-    employeesData.push(
-    {
-        fullName: faker.person.fullName(),
-        jobTitle: faker.person.jobTitle(),
-        phoneNumber: faker.phone.number(),
-        email: faker.internet.email(),
-        address: faker.location.streetAddress(),
-        city: faker.location.city(),
-        state: faker.location.state(),
-        contacts: faker.helpers.arrayElements(contacts, 2),
-      }
-  );
-}
-const empoloyees = faker.helpers.arrayElements(employeesData, 3);
+const empoloyees = faker.helpers.multiple(createRandomEmployeeWithContacts, { count: 3 });
 
 module.exports = empoloyees;
