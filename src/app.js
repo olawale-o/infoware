afdc3e73f49f13c1944c2d9df7fa5567a758d863
olawale-o <omoogunolawale@gmail.com>
2023-05-29 03:37:26 +0100

    Use proxy server

--- src/app.js
+++ src/app.js
@@ -1,12 +1,42 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+const { createProxyMiddleware } = require('http-proxy-middleware')
 const routeHandler = require("./route");
 const handleError = require('./common/error-handler');
+
 const app = express();
+const filter = function (pathname, req) {
+  return pathname.match('^/api/employees') && req.method === 'GET';
+}
+const proxyOptions = {
+  target: 'http://localhost:6000',
+  changeOrigin: true,
+  pathRewrite: {
+    '^/api/employees': '/api/v1/employees',
+  },
+  router: {
+    'localhost:5000': 'http://localhost:5000',
+  },
+  onError: (err, req, res, target) => {
+    console.log("err", err);
+  },
+  onProxyReq: (proxyReq, req, res) => {
+    if (req.body) {
+      let bodyData = JSON.stringify(req.body);
+      // incase if content-type is application/x-www-form-urlencoded -> we need to change to application/json
+      proxyReq.setHeader('Content-Type','application/json');
+      proxyReq.setHeader('Content-Length', Buffer.byteLength(bodyData));
+      // stream the content
+      proxyReq.write(bodyData);
+    }
+  },
+};
+const proxy = createProxyMiddleware(filter, proxyOptions);
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(express.json());
+app.use('/api/employees', proxy);
 
 routeHandler(app);
 
